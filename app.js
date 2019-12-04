91f7392ca5fefc05eba67080aa139660e2d6d6cd
Md Nazmul Hossain <bilashcse@gmail.com>
2019-12-04 13:58:00 +0600

    list of repos, filter forked, trying to delete

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const rp = require('request-promise');
+
+const configs = require('./configs/default.json');
+
+const app = express();
+
+app.listen(3030, () => console.log('Server started on port 3030'));
+
+
+const findForkedRepos = (repos) => repos.filter((r) => r.fork === true);
+
+const allRepos = async () => {
+  const options = {
+    uri: `https://api.github.com/users/${configs.userName}/repos`,
+    qs: {
+      access_token: configs.accessToken,
+    },
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    },
+    json: true,
+  };
+
+  const results = await rp(options);
+  const repos = results.map((r) => ({
+    name: r.name,
+    full_name: r.full_name,
+    fork: r.fork,
+    forks: r.fork,
+    language: r.language,
+  }));
+
+  const forked = findForkedRepos(repos);
+  return forked;
+};
+
+
+const deleteForedRepo = async (repo) => {
+  console.log(`Deleting repo : ${repo.name}`);
+
+  try {
+    const options = {
+      method: 'POST',
+      uri: `https://api.github.com/repos/${repo.full_name}`,
+      header: {
+        access_token: configs.accessToken,
+      },
+      headers: {
+        'User-Agent':
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+      },
+      json: true,
+    };
+    await rp(options);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+(async function init() {
+  const forkedRepos = await allRepos();
+  console.log(forkedRepos);
+
+  if (forkedRepos.length > 0) {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < forkedRepos.length; i++) {
+    // eslint-disable-next-line no-await-in-loop
+      await deleteForedRepo(forkedRepos[i]);
+    }
+  }
+}());
